a21fec96d0ebbe264100cee7a12ec45ab7cb9044
andrea <andreawscotti@gmail.com>
2022-06-02 00:09:10 +0200

    spa

--- src/frontend/Spa.jsx
+++ src/frontend/Spa.jsx
@@ -1,4 +1,4 @@
-function App(){
+function Spa(){
     return (
         <div>
             ciao mondo
@@ -6,4 +6,4 @@ function App(){
     );
 }
 
-export default App
+export default Spa
--- src/frontend/main.jsx
+++ src/frontend/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App.jsx";
+import Spa from "./Spa.jsx";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("spa"));
