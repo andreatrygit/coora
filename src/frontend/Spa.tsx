7498f23a3578ae7750f2b1d99f91577b68fbada1
andrea <andreawscotti@gmail.com>
2022-06-12 00:08:02 +0200

    react router works

--- src/frontend/Spa.tsx
+++ src/frontend/Spa.tsx
@@ -2,19 +2,35 @@ import React from "react";
 import { useState } from 'react';
 import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core';
 
+import ReactDOM from "react-dom/client";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+import {Home} from "./components/routes/_/Home";
+
 function Spa(){
   const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
   const toggleColorScheme = (value?: ColorScheme) =>
     setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
 
   return (
+
     <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
       <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
-       
-      <div>ciao mondo</div>
+
+        <BrowserRouter>
+
+          <Routes>
+
+            <Route path="/" element={<Home />}/>
+           
+
+          </Routes>
+
+        </BrowserRouter>
 
       </MantineProvider>
     </ColorSchemeProvider>
+
   );
 }
 
---
+++ src/frontend/components/routes/_/Home.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+export function Home(props){
+    return(
+        <div>Ciao mondo</div>
+    )
+}
