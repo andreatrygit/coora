16df87f62d89c51d0b706280fb75d5584128ce96
andrea <andreawscotti@gmail.com>
2022-06-14 00:19:55 +0200

    Hero perfect

--- src/frontend/components/misc/Hero.jsx
+++ src/frontend/components/misc/Hero.jsx
@@ -1,15 +1,25 @@
 import React from "react";
+import { Stack } from "@mantine/core";
 import {Text} from "@mantine/core";
 
 export function Hero(props){
     return(
-        <Text
-            component="span"
-            align="center"
-            variant="gradient"
-            gradient={{ from: 'indigo', to: 'cyan', deg: 45 }}
-            weight={700}
-            style={{fontSize:'3.75rem'}}
-            >{props.title}</Text>
+        <Stack align="center" justify="center" spacing="xs">
+            <Text
+                component="div"
+                align="center"
+                variant="gradient"
+                gradient={{ from: 'indigo', to: 'cyan', deg: 45 }}
+                weight={700}
+                style={{fontSize:'3.75rem', wordBreak:'normal'}}
+                >{props.title}</Text>
+            <Text 
+                component="div"
+                align="center"
+                color="dimmed"
+                weight={400}
+                style={{fontSize:'2rem', wordBreak:'normal'}}
+                >{props.message}</Text>
+        </Stack>
     )
 }
--- src/frontend/components/routes/_/Home.jsx
+++ src/frontend/components/routes/_/Home.jsx
@@ -6,7 +6,7 @@ import {Hero} from "../../misc/Hero";
 export function Home(props){
     return(
         <Center style={{width:'100vw', height:'100vh'}}>
-            <Hero title="Coora"></Hero>
+            <Hero title="Coora" message="La piattaforma Open Source per la cura delle persone."></Hero>
         </Center>
     )
 }
