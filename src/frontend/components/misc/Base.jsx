08248cbd596a59472ff4914043d9c69d20079b5d
andrea <andreawscotti@gmail.com>
2022-06-17 23:21:11 +0200

    cookie policy OK

--- src/frontend/components/misc/Base.jsx
+++ src/frontend/components/misc/Base.jsx
@@ -1,18 +1,21 @@
 import React, { useEffect } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useMatch } from "react-router-dom";
 import { kyPost } from "../../utils";
 import { showNotification } from "@mantine/notifications";
 
 
 export function Base(){
     useEffect(()=>{
-        let cookiePolicy = false;
+        console.log('effect')
         const message = "Questo sito utilizza alcuni cookie tecnici, solamente tecnici. Se non li desideri, interrompi la navigazione e rimuovili manualmente dalla cache di questo browser. Altrimenti, proseguendo la navigazione, accetti il loro uso."
-        kyPost('/website/is-cookie-policy-set',({value})=>cookiePolicy=(value==='yes'),()=>{});
-        if(!cookiePolicy){
-            showNotification({title:"INFORMATIVA COOKIE", message:message});
-        }
-    }
+        kyPost('/website/is-cookie-policy-set',
+                ({value})=>{
+                    if(value==='no'){
+                        showNotification({title:"INFORMATIVA COOKIE", message:message, autoClose:false});
+                    }
+                },
+                ()=>{});
+    } 
     );
     return (
         <Outlet/>
