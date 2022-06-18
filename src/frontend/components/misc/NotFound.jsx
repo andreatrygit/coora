30751a79129a24637df03c001de1b686af96c120
andrea <andreawscotti@gmail.com>
2022-06-19 00:26:10 +0200

    notfound click handler -> No blue rectangle

--- src/frontend/components/misc/NotFound.jsx
+++ src/frontend/components/misc/NotFound.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import { Center } from "@mantine/core";
 import { Stack } from "@mantine/core";
 import { Hero } from "./Hero";
-import { Link} from "react-router-dom";
 import { Button } from "@mantine/core";
+import { useNavigate } from "react-router-dom";
 
 export function NotFound(props){
+    let navigate = useNavigate();
+    function goHome(){navigate("/")}
     return(
         <Center style={{width:'100vw', height:'100vh'}}>
             <Stack align="center" justify="center" spacing="xs">
                 <Hero title="Qui non c'è nulla" message="Ci dispiace veramente!"></Hero>
-                <Link to={"/"}><Button size="xl" radius={"xl"}>Pagina Iniziale</Button></Link>
+                <Button size="xl" radius={"xl"} onClick={goHome}>Pagina Iniziale</Button>
             </Stack>
         </Center>
     )
