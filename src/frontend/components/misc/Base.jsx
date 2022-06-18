6f4511aaeb1d0d0616c848fa0ab3f5b0222e120c
andrea <andreawscotti@gmail.com>
2022-06-19 00:02:19 +0200

    Base clean

--- src/frontend/components/misc/Base.jsx
+++ src/frontend/components/misc/Base.jsx
@@ -6,7 +6,6 @@ import { showNotification } from "@mantine/notifications";
 
 export function Base(){
     useEffect(()=>{
-        console.log('effect')
         const message = "Questo sito utilizza alcuni cookie tecnici, solamente tecnici. Se non li desideri, interrompi la navigazione e rimuovili manualmente dalla cache di questo browser. Altrimenti, proseguendo la navigazione, accetti il loro uso."
         kyPost('/website/is-cookie-policy-set',
                 ({value})=>{
