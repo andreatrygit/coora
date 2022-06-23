d3e2ebdff9298abc198c7a9ab78d4e55def96df1
andrea <andreawscotti@gmail.com>
2022-06-23 23:04:34 +0200

    no usenavigate

--- src/frontend/components/misc/NotFound.jsx
+++ src/frontend/components/misc/NotFound.jsx
@@ -3,16 +3,14 @@ import { Center } from "@mantine/core";
 import { Stack } from "@mantine/core";
 import { Hero } from "./Hero";
 import { Button } from "@mantine/core";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export function NotFound(props){
-    let navigate = useNavigate();
-    function goHome(){navigate("/")}
     return(
         <Center style={{width:'100vw', height:'100vh'}}>
             <Stack align="center" justify="center" spacing="xs">
                 <Hero title="Qui non c'è nulla" message="Ci dispiace veramente!"></Hero>
-                <Button size="xl" radius={"xl"} onClick={goHome}>Pagina Iniziale</Button>
+                <Button component={Link} to="/" size="xl" radius={"xl"}>Pagina Iniziale</Button>
             </Stack>
         </Center>
     )
