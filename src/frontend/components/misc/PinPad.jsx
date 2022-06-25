eb03ddaa613224264eb30b2cde34478c574bdabc
andrea <andreawscotti@gmail.com>
2022-06-26 00:15:24 +0200

    nearly

--- src/frontend/components/misc/PinPad.jsx
+++ src/frontend/components/misc/PinPad.jsx
@@ -66,7 +66,7 @@ const pinPadMachine = createMachine({
         start:(context,event)=>{assign({timeoutHandle:(context,event)=>{setTimeout(()=>{send('RESET')},3000)}})},
         stop:(context,event)=>{if(context.timeoutHandle){clearTimeout(context.timeoutHandle)};assign({timeoutHandle:null})},
         reset:(context,event)=>{assign({pin:''})},
-        addDigit:(context,event)=>{assign({pin:(context,event)=>{context.pin + event.digit}})},
+        addDigit:(context,event)=>{assign({pin:(context,event)=>{context.pin + event.digit}});console.log(event.digit,context.pin)},
         deliver:(context,event)=>{onPin(context.pin)},
         exit:(context,event)=>{onCancel()}
     }
@@ -79,15 +79,15 @@ export function PinPad({onPin,onCancel}){
         <Stack align="center" justify="center">
             <Badge size="xl" radius="xl">{state.context.pin}</Badge>
             <Grid columns={13} gutter={"xs"}>
-                <Grid.Col span={3} offset={1}><Center><Button size="xl" radius="xl" onClick={event=>send({type:'DIGIT',digit:'1'})}>1</Button></Center></Grid.Col>
-                <Grid.Col span={3} offset={1}><Center><Button size="xl" radius="xl" onClick={event=>send({type:'DIGIT',digit:'2'})}>2</Button></Center></Grid.Col>
-                <Grid.Col span={3} offset={1}><Center><Button size="xl" radius="xl" onClick={event=>send({type:'DIGIT',digit:'3'})}>3</Button></Center></Grid.Col>
-                <Grid.Col span={3} offset={1}><Center><Button size="xl" radius="xl" onClick={event=>send({type:'DIGIT',digit:'4'})}>4</Button></Center></Grid.Col>
-                <Grid.Col span={3} offset={1}><Center><Button size="xl" radius="xl" onClick={event=>send({type:'DIGIT',digit:'5'})}>5</Button></Center></Grid.Col>
-                <Grid.Col span={3} offset={1}><Center><Button size="xl" radius="xl" onClick={event=>send({type:'DIGIT',digit:'6'})}>6</Button></Center></Grid.Col>
-                <Grid.Col span={3} offset={1}><Center><Button size="xl" radius="xl" onClick={event=>send({type:'DIGIT',digit:'7'})}>7</Button></Center></Grid.Col>
-                <Grid.Col span={3} offset={1}><Center><Button size="xl" radius="xl" onClick={event=>send({type:'DIGIT',digit:'8'})}>8</Button></Center></Grid.Col>
-                <Grid.Col span={3} offset={1}><Center><Button size="xl" radius="xl" onClick={event=>send({type:'DIGIT',digit:'9'})}>9</Button></Center></Grid.Col>
+                <Grid.Col span={3} offset={2}><Center><Button size="xl" radius="xl" onClick={event=>send({type:'DIGIT',digit:'1'})}>1</Button></Center></Grid.Col>
+                <Grid.Col span={3}><Center><Button size="xl" radius="xl" onClick={event=>send({type:'DIGIT',digit:'2'})}>2</Button></Center></Grid.Col>
+                <Grid.Col span={3}><Center><Button size="xl" radius="xl" onClick={event=>send({type:'DIGIT',digit:'3'})}>3</Button></Center></Grid.Col>
+                <Grid.Col span={3} offset={2}><Center><Button size="xl" radius="xl" onClick={event=>send({type:'DIGIT',digit:'4'})}>4</Button></Center></Grid.Col>
+                <Grid.Col span={3}><Center><Button size="xl" radius="xl" onClick={event=>send({type:'DIGIT',digit:'5'})}>5</Button></Center></Grid.Col>
+                <Grid.Col span={3}><Center><Button size="xl" radius="xl" onClick={event=>send({type:'DIGIT',digit:'6'})}>6</Button></Center></Grid.Col>
+                <Grid.Col span={3} offset={2}><Center><Button size="xl" radius="xl" onClick={event=>send({type:'DIGIT',digit:'7'})}>7</Button></Center></Grid.Col>
+                <Grid.Col span={3}><Center><Button size="xl" radius="xl" onClick={event=>send({type:'DIGIT',digit:'8'})}>8</Button></Center></Grid.Col>
+                <Grid.Col span={3}><Center><Button size="xl" radius="xl" onClick={event=>send({type:'DIGIT',digit:'9'})}>9</Button></Center></Grid.Col>
                 <Grid.Col span={3} offset={5}><Center><Button size="xl" radius="xl" onClick={event=>send({type:'DIGIT',digit:'0'})}>0</Button></Center></Grid.Col>
             </Grid>
             <Button size="xl" radius="xl" onClick={event=>send('CANCEL')}>Annulla</Button>
