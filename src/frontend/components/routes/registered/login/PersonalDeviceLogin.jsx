ce795350f208a78b09de4bccfba351c880576d49
andrea <andreawscotti@gmail.com>
2022-07-01 23:58:29 +0200

    personal device login continued

--- src/frontend/components/routes/registered/login/PersonalDeviceLogin.jsx
+++ src/frontend/components/routes/registered/login/PersonalDeviceLogin.jsx
@@ -1,5 +1,7 @@
 import { Stack } from "@mantine/core";
+import { showNotification } from "@mantine/notifications";
 import {React, useState, useEffect} from "react";
+import { isValidQrCode, isValidTimeClockQrCode } from "../../../../utils";
 import { QrCodeScanner } from "../../../misc/QrCodeScanner";
 
 export function PersonalDeviceLogin(){
@@ -16,14 +18,29 @@ export function PersonalDeviceLogin(){
             if(state==='personal-login'){
                 doPersonalLogin();
             }
-            if(state==='shared-device-login'){
+            if(state==='shared-device-login-2'){
                 doSharedDeviceLogin();
             }
         }
     })
 
     useEffect(()=>{
-        
+        if(qrCode){
+            if(!isValidQrCode(qrCode)){
+                showNotification({title:'QRCODE NON VALIDO.', message:'Stai scansionando un codice non creato da noi.', color:'yellow', autoClose:false});
+                setQrCode('');
+            }
+            if(isValidTimeClockQrCode(qrCode)){
+                if(state==='time-clocking'){
+                    setState('start');
+                    doTimeClock()
+                }
+                else{
+                    showNotification({title:'QRCODE DA TIMBRATORE.', message:'Stai scansionando un codice da timbratore anziché da postazione condivisa.', color:'yellow', autoClose:false});
+                    setQrCode('');
+                }
+            }
+        }
     })
 
     return(
